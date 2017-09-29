c9a8bd32101bf4eaaae367321c35db3f19c4a166
Ryo Ito <r-ito@virtualex.co.jp>
2017-09-29 17:51:27 +0900

    Dataframe保存対応

--- data_get.py
+++ data_get.py
@@ -1,18 +1,17 @@
-
-from bs4 import BeautifulSoup
 import numpy as np
+import pandas as pd
 import urllib.request
+import pickle
 import re
 import os
+import math
+from sklearn import metrics
 from sklearn.externals import joblib
 from sklearn import preprocessing
 from sklearn import linear_model
-from sklearn import metrics
-import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV
-import math
-import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout,LSTM
 from keras import regularizers
@@ -26,9 +25,17 @@ def remove_td(data):
     clean_data = list(map(int, clean_data))
     return clean_data
 
+def make_query():
+    now = datetime.now()
+    year = now.year
+    month = now.month
+    day = now.day
+    url_ = "https://info.finance.yahoo.co.jp/history/?code=6193.T&sy=2016&sm=1&sd=1&ey=" + year + "&em=" + month + "&ed=" + day + "&tm=d&p=1" 
+    return url_
+
 def make_url():
     urls = []
-    url_ = "https://info.finance.yahoo.co.jp/history/?code=6193.T&sy=2016&sm=1&sd=1&ey=2017&em=9&ed=21&tm=d&p="
+    url_ = make_query()
     _url = [str(i) for i in range(1,20)]
     for _ in _url:
         url = url_ + _
@@ -83,23 +90,73 @@ def make_data():
 
     return sum_first, sum_top, sum_tail, sum_last
 
-def convert_array_concat( sum_data):
+def convert_array_concat(sum_data):
     sum_data_arr = np.array(sum_data)
     f = sum_data_arr[0]
     for i in range(1, sum_data_arr.shape[0]):
         f = np.append(f,sum_data_arr[i])
     return f
 
-def make_y_data(sum_tail):
+def first_make_data():
     
-    yy = []
-    y = sum_tail
-    last = y[-1]
-    first = y[0]
-    yy.append([y[i - 1] for i in range(0, y.shape[0]-1)])
-    yy = np.append(yy, last)
+    #初めてデータを取得する際に使用するメソッド    
+    str_d = get_date()
+    output_file_path = ".\\data\\" + str_d + "vxc_stock"
 
-    return yy
+    #csv file入手先
+    data_2016 = pd.read_csv(".\data\stocks_6193-T_1d_2016.csv", sep="," ,encoding = "SHIFT-JIS").sort_index(ascending=False)
+    data_2017 = pd.read_csv(".\data\stocks_6193-T_1d_2017.csv", sep="," ,encoding = "SHIFT-JIS").sort_index(ascending=False)
+    data_vxc = pd.concat([data_2016, data_2017])
+    #DataFrameをファイルに保存
+    data_vxc.to_picle(output_file_path) 
+    #DataFrameからデータを抽出
+    
+    date, head, hign, low, tail = extract_data(data_vxc)
+
+    return date, head, hign, low, tail
+
+def extract_data(data):
+    
+    date = np.array(data['日付'])
+    head = np.array(data['始値'])
+    hign = np.array(data['高値'])
+    low = np.array(data['安値'])
+    tail = np.array(data['終値'])
+
+    return date, head, hign, low, tail
+
+def get_new_data():
+
+    str_d = get_date()
+    input_file_path = '.\\data\\' + str_d + "vxc_stock"
+    output_file_path = '.\\data\\' + str_d + "vxc_stock"
+    #下記のURLから直近250日のCSVデータを取得する
+    url  =  "http://k-db.com/stocks/6193-T_download=csv"
+    urllib.request.urlretrieve(url, '.\data\new_stocks.csv')
+    #取得したCSVデータをDATAFRAME化し、既存のDATAFRAMEと結合する
+    data_new = pd.read_csv(".\data\new_stocks.csv", sep="," ,encoding = "SHIFT-JIS")
+    new_line = data_new[0:1]
+
+    with open(input_file_path, 'rb'):
+        oldDataFrame= pickle.load()
+
+    new_vxc = pd.concat([oldDataFrame, new_line])
+    #結合したDATAFRAMEを再度保存する
+    new_vxc.to_picle(output_file_path) 
+    #各インデックスのデータの取得
+    date, head, hign, low, tail = extract_data(new_vxc)
+
+
+def make_y_data(sum_last):
+    
+    y_data =[]
+    train = np.array(sum_last)
+    last = train[-1]
+    first = train[0]
+    y_data.append([train[i + 1] for i in range(0, train.shape[0]-1)])
+    y_data = np.append(y_data, first)
+
+    return y_data
 
 def stack_data(sum_first, sum_top, sum_tail, sum_last):
     
@@ -122,7 +179,7 @@ def lasso_regressoin(train_data_x, train_data_y ,m_):
     model = linear_model.Lasso(alpha = 0.0001)
     model.fit(train_data_x, train_data_y)
     
-    joblib.dump(model, os.path.join(m_,"lasso.pa ==kl"))
+    joblib.dump(model, os.path.join(m_,"lasso.pkl"))
     
 def linear_regression(train_data_x, train_data_y ,m_):
     print("Training linear regression")
@@ -177,40 +234,49 @@ def long_short_term_memory(train_data_x, train_data_y ,m_):
         loss = "mse")
     lstm.fit(rnn_train_x, train_data_y, epochs=15, batch_size=7, validation_split=0.1)
     json_string = lstm.to_json()
-    open(os.path.join(m_,'lstm_model.json'),'w').write(json_string)
-    lstm.save_weights(os.path.join(m_,'lstm_model_weights.hdf5'))
+    open(os.path.join(m_,'rnn_model.json'),'w').write(json_string)
+    lstm.save_weights(os.path.join(m_,'rnn_model_weights.hdf5'))
 
-  
+def get_date():
+    """
+    本日の日付をStringでGETする
+    """
+    d = date.today()
+    str_d = str(d)
+    return str_d
+
 if __name__ == '__main__':
     
     d_ = ".\data"
     m_ = ".\model"
 
-    d = date.today()
-    str_d = str(d)
+    str_d = get_date()
 
     y_data_file_name = str_d + "_y_data"
     X_data_file_name = str_d + "_X_data"
 
-    y_test_data_file_name = str_d + "_y_test_data"
-    X_test_data_file_name = str_d + "_X_test_data"
+    y_test_data_file_name = str_d + "_y_test_data.npy"
+    X_test_data_file_name = str_d + "_X_test_data.npy"
 
     try:
-        y_data = np.load(os.path.join(d_, y_data_file_name+".npy"))
-        X_data = np.load(os.path.join(d_, X_data_file_name+".npy"))    
+        y_data = np.load(os.path.join(d_, y_data_file_name))
+        X_data = np.load(os.path.join(d_, X_data_file_name))    
     
     except:
-        sum_first, sum_top, sum_tail, sum_last = make_data()
-        s_list = [sum_first, sum_top, sum_tail, sum_last]
-        sum_first = convert_array_concat(sum_first)
-        sum_top = convert_array_concat(sum_top)
-        sum_tail = convert_array_concat(sum_tail)
-        sum_last = convert_array_concat(sum_last)
+        #sum_first, sum_top, sum_tail, sum_last = make_data()
+        #s_list = [sum_first, sum_top, sum_tail, sum_last]
+        #date, sum_first, sum_top, sum_tail, sum_last = get_new_data()
+        #############################################################
+        date, sum_first, sum_top, sum_tail, sum_last = make_datas()#
+        #############################################################
+        #sum_first = convert_array_concat(sum_first)
+        #sum_top = convert_array_concat(sum_top)
+        #sum_tail = convert_array_concat(sum_tail)
+        #sum_last = convert_array_concat(sum_last)
         
-        y_data = make_y_data(sum_tail)
+        y_data = make_y_data(sum_last)
         X_data = stack_data(sum_first, sum_top, sum_tail, sum_last)
 
-          
         np.save(os.path.join(d_,y_data_file_name), y_data)
         np.save(os.path.join(d_,X_data_file_name), X_data)
 
@@ -219,22 +285,26 @@ if __name__ == '__main__':
         X_scaled = preprocessing.scale(X_data)
         X_scaled.std(axis=0)
 
-        split_num = 0.2
+        split_num = 0.8
         ratio = int(X_scaled.shape[0] * split_num)
-        train_data_x = X_scaled[:][ratio:]
-        test_data_x = X_scaled[:][:ratio]
-        yy = make_y_data(sum_tail)
-        train_data_y = yy[ratio:]
-        test_data_y = yy[:ratio]
-            
+        train_data_x = X_scaled[:][:ratio]
+        test_data_x = X_scaled[:][ratio:]
+        
+        train_data_y = y_data[:ratio]
+        test_data_y = y_data[ratio:]
+
+        dnn_train_data_x = X_data[:][:ratio]
+        dnn_test_data_x = X_data[:][ratio:]
+
         np.save(os.path.join(d_,y_test_data_file_name), test_data_y)
         np.save(os.path.join(d_,X_test_data_file_name), test_data_x)
+        np.save(os.path.join(d_,"DNN_"+X_test_data_file_name), dnn_test_data_x)
 
-          #y_classify = [1  if sum_last[i + 1] > sum_last[i] else 0 for i in range(sum_last.shape[0] - 1)]
+        #y_classify = [1  if sum_last[i + 1] > sum_last[i] else 0 for i in range(sum_last.shape[0] - 1)]
         #if sum_last[-1] > sum_last[0]:
-        #    y_classify = np.append(y_classify, 1)
-            #else:
-         #    y_classify = np.append(y_classify, 0)
+        #y_classify = np.append(y_classify, 1)
+        #else:
+        #y_classify = np.append(y_classify, 0)
         #train_data_y = y_classify[ratio:]
         #test_data_y = y_classify[:ratio]
         train_data_y.reshape(-1)
@@ -244,8 +314,8 @@ if __name__ == '__main__':
         lasso_regressoin(train_data_x, train_data_y, m_)
         linear_regression(train_data_x, train_data_y, m_)
         support_vecter_regression(train_data_x, train_data_y, m_)
-        feed_forward_neural_network(train_data_x, train_data_y, m_)
-        long_short_term_memory(train_data_x, train_data_y, m_)
+        feed_forward_neural_network(dnn_train_data_x, train_data_y, m_)
+        long_short_term_memory(dnn_train_data_x, train_data_y, m_)
         
 
 
